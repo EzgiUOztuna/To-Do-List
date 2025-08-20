7ba25d97da7a3c60abbaff1ffbb2a7a0deb6a3e8
EzgiUOztuna <ezgiuygn@gmail.com>
2025-08-20 16:15:28 +0300

    Update toDoReducer.js

--- src/store/reducers/toDoReducer.js
+++ src/store/reducers/toDoReducer.js
@@ -1,11 +1,11 @@
-import { ADD_TASK, COMPLETED_TASK, DELETE_TASK, SET_TASKS, TASK_IN_PROGRESS } from "../actions/toDoActions";
+import { ADD_TASK, COMPLETED_TASK, DELETE_TASK, SET_TASK, TASK_IN_PROGRESS } from "../actions/toDoActions";
 
 export const initialState = [];
 
 export const toDoReducer = (state = initialState, action) => {
     switch (action.type) {
-        case SET_TASKS:
-            return [...state, action.payload]
+        case SET_TASK:
+            return action.payload;
         //case ADD_TASK:
         //    return [...state, action.payload];
         //case DELETE_TASK:
