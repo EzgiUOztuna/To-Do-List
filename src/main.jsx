02baeeb363a6021bd82a550a557e7da24bfb811c
EzgiUOztuna <ezgiuygn@gmail.com>
2025-08-20 14:07:54 +0300

    wrapped with Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { myStore } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={myStore}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </Provider>
 )
