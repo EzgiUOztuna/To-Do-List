60dc9475b370b9e5b9c2f23093d37c0e6e6a2fd9
EzgiUOztuna <ezgiuygn@gmail.com>
2025-08-20 16:15:26 +0300

    Update toDoActions.js

--- src/store/actions/toDoActions.js
+++ src/store/actions/toDoActions.js
@@ -1,10 +1,22 @@
-export const SET_TASKS = "set task";
+import axios from "axios";
+
+export const SET_TASK = "set task";
 export const ADD_TASK = "add task";
 export const DELETE_TASK = "delete task";
 export const COMPLETED_TASK = "completed task";
 export const TASK_IN_PROGRESS = "task inprogress";
 
-export function setTasks(tasks) {
+/*export function setTasks(tasks) {
     return { type: SET_TASKS, payload: tasks };
+}*/
+
+export const setTask = () => (dispatch) => {
+    axios
+        .get("https://68a2c82bc5a31eb7bb1dc243.mockapi.io/api/v1/tasks")
+        .then((res) => {
+            const setTask = { type: SET_TASK, payload: res.data }
+            dispatch(setTask);
+        })
+        .catch((error) => console.error(error))
 }
 
