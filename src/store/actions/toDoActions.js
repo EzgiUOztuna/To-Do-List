2a3044274e9bb068397f8f6f191d660e19c6a8eb
EzgiUOztuna <ezgiuygn@gmail.com>
2025-08-20 15:37:05 +0300

    Update toDoActions.js

--- src/store/actions/toDoActions.js
+++ src/store/actions/toDoActions.js
@@ -1,4 +1,10 @@
+export const SET_TASKS = "set task";
 export const ADD_TASK = "add task";
 export const DELETE_TASK = "delete task";
 export const COMPLETED_TASK = "completed task";
 export const TASK_IN_PROGRESS = "task inprogress";
+
+export function setTasks(tasks) {
+    return { type: SET_TASKS, payload: tasks };
+}
+
